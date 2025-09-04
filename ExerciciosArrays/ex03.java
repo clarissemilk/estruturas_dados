73fcb8b2f3d053e5636ac0df3407675927a91e2d
clarissemilk <clarissesantana002@gmail.com>
2025-09-04 16:35:15 -0300

    questão 3 da lista de arrays

---
+++ ExerciciosArrays/ex03.java
@@ -0,0 +1,32 @@
+/*  Faça um programa em Java que leia 4 notas,
+ mostre as notas e a média na tela */
+package ExerciciosArrays;
+
+import java.util.Scanner;
+
+public class ex03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] notas = new double[4];
+        double soma = 0;
+
+        System.out.println("Digite 4 notas:");
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Nota " + (i + 1) + ": ");
+            notas[i] = sc.nextDouble();
+            soma += notas[i];
+        }
+
+        System.out.println("\nNotas digitadas:");
+        for (double n : notas) {
+            System.out.println(n);
+        }
+
+        // Calculando e mostrando a média
+        double media = soma / notas.length;
+        System.out.println("\nMédia = " + media);
+
+        sc.close();
+    }
+}
